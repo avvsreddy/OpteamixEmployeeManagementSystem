6f460e9ee07ffdc6ace85f4d1f483fdeee4b5205
PUYadav <PUYadav@OPT-LAP-0068>
2026-06-02 15:11:23 +0530

    Created DBContext

---
+++ OpteamixEmployeeManagementSystem.Data/EmployeeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpteamixEmployeeManagementSystem.Data
+{
+    internal class EmployeeDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Integrated Security=True; Initial Catalog=OpteamixEmployeeDbComplex;");
+        }
+        
+    }
+}
