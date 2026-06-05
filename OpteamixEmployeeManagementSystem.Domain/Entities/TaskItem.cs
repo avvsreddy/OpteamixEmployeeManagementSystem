bb3f02b79254452e8473f4b601e24be82eab99d2
Ganesh <ganeshpalar17@gmail.com>
2026-06-05 16:00:20 +0530

    Updated

--- OpteamixEmployeeManagementSystem.Domain/Entities/TaskItem.cs
+++ OpteamixEmployeeManagementSystem.Domain/Entities/TaskItem.cs
@@ -1,5 +1,6 @@
 ﻿using OpteamixEmployeeManagementSystem.Domain.Enums;
 using System.ComponentModel.DataAnnotations;
+
 namespace OpteamixEmployeeManagementSystem.Domain.Entities
 {
     public class TaskItem
@@ -7,16 +8,19 @@ namespace OpteamixEmployeeManagementSystem.Domain.Entities
         [Key]
         public int TaskId { get; set; }
 
+        [Required]
         public string Title { get; set; } = string.Empty;
 
+        [Required]
         public string Description { get; set; } = string.Empty;
 
         public TaskItemStatus Status { get; set; }
+            = TaskItemStatus.New;
 
         public TaskPriority Priority { get; set; }
 
         // Future FK with Employee Module
-        public int EmployeeId { get; set; }
+        public int? EmployeeId { get; set; }
 
         // Future FK with Project Module
         public int ProjectId { get; set; }
